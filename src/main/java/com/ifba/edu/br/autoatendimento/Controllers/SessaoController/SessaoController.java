8f1488e682c38d81fd42a3116852c81b287b6bf8
lucassouza31 <lucaseasy10@gmail.com>
2023-02-10 11:39:53 -0300

    Tela de Sessão

--- src/main/java/com/ifba/edu/br/autoatendimento/Controllers/SessaoController/SessaoController.java
+++ src/main/java/com/ifba/edu/br/autoatendimento/Controllers/SessaoController/SessaoController.java
@@ -1,9 +1,15 @@
 package com.ifba.edu.br.autoatendimento.Controllers.SessaoController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class SessaoController {
     
+    @GetMapping("/cadastrarSessao")
+    public String sessaoCadastrar() {
+        return "Sessao/sessao";
+    }
 
 
 
