db43a3b8e1d28bc2a5c81447c431c044da1edf6a
Lucas <76852058+LucasSouza31@users.noreply.github.com>
2023-02-27 19:46:29 -0300

    Ajuste no Controller para salvar novo usuário

--- src/main/java/br/edu/ifba/saj/autoatendimento/controllers/pessoaController/PessoaController.java
+++ src/main/java/br/edu/ifba/saj/autoatendimento/controllers/pessoaController/PessoaController.java
@@ -4,11 +4,26 @@ package br.edu.ifba.saj.autoatendimento.controllers.pessoaController;
 @Controller
 public class PessoaController {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
+
+    // @GetMapping("/cadastrarPessoa")
+    // public String CadastrarProduto() {
+    //     return "usuario/cadastrarUsuario";
+    // }
+
     @GetMapping("/cadastrarPessoa")
-    public String CadastrarProduto() {
+    public String novoUsuario(Model model) {
+        model.addAttribute("pessoa", new Pessoa());
         return "usuario/cadastrarUsuario";
     }
 
+    @PostMapping("/cadastrarEstabelecimento")
+    public String cadastrarEstabelecimento(@ModelAttribute("pessoa") Pessoa pessoa) {
+        usuarioService.salvar(pessoa);
+        return "redirect:/cadastrarUsuario";
+    }
 
 
 
