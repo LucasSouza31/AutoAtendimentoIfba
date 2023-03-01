1351179e62c0de54802c7b20660d10efaffcbe03
lucassouza31 <lucaseasy10@gmail.com>
2023-03-01 16:30:10 -0300

    Ajuste no código

--- src/main/java/br/edu/ifba/saj/autoatendimento/model/ProdutoIndustrial.java
+++ src/main/java/br/edu/ifba/saj/autoatendimento/model/ProdutoIndustrial.java
@@ -12,6 +12,9 @@ import lombok.Setter;
 
 public class ProdutoIndustrial extends AbstractEntity {
 
+    @Nonnull
+    @ManyToOne
+    private Estabelecimento estabelecimento;
 
     @Nonnull
     private String nomeProdutoIndustrial;
