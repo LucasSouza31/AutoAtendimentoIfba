be4e08d7dbcf81bc59dd42143a2054e8abba5aee
lucassouza31 <lucaseasy10@gmail.com>
2023-03-03 19:20:06 -0300

    Ajustes

--- src/main/java/br/edu/ifba/saj/autoatendimento/model/Pessoa.java
+++ src/main/java/br/edu/ifba/saj/autoatendimento/model/Pessoa.java
@@ -1,9 +1,12 @@
 package br.edu.ifba.saj.autoatendimento.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,13 +31,15 @@ public class Pessoa extends AbstractEntity{
     
     private String occupation;   
 
+     @OneToMany(fetch = FetchType.LAZY, mappedBy = "proprietario")
+     private Set<Estabelecimento> estabelecimento;
+
     // @Enumerated(EnumType.STRING)
     // @ElementCollection(fetch = FetchType.EAGER)
     // @Nonnull
     // private Set<Role> roles;
     
-    // @OneToMany(fetch = FetchType.LAZY, mappedBy = "proprietario")
-    // private Set<Estabelecimento> estabelecimento;
+
 
 
 
